aaddd55b2dfb0ee76eea541dbd52d40432c7366e
2114550 <2114550@talnet.nl>
2019-09-26 09:25:44 +0200

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,12 @@
+<?php
+$b = 16.5;
+echo "Wat is je leeftijd?".PHP_EOL;
+$a = readline();
+if ($a > $b)
+{
+echo "Je bent oud genoeg!".PHP_EOL;
+}
+if ($a < $b)
+{
+echo "sorry, je bent te jong".PHP_EOL;
+}
